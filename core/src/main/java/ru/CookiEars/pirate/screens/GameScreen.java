989e19e72c6282f408fc892c0e49920966fdc783
salahetdinovartyom <210269813+salahetdinovartyom@users.noreply.github.com>
2026-08-02 23:28:03 +0300

    v3.2

--- core/src/main/java/ru/CookiEars/pirate/screens/GameScreen.java
+++ core/src/main/java/ru/CookiEars/pirate/screens/GameScreen.java
@@ -1,9 +1,32 @@
 package ru.CookiEars.pirate.screens;
 
-import ru.CookiEars.pirate.MyGdxGame;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 
-public class GameScreen extends BaseScreen{
+import ru.CookiEars.pirate.MyGdxGame;
+import ru.CookiEars.pirate.utils.Level;
+
+public class GameScreen extends BaseScreen {
+    private OrthoCachedTiledMapRenderer mapRenderer;
+    private Level level;
     public GameScreen(MyGdxGame myGdxGame) {
         super(myGdxGame);
     }
+
+    @Override
+    public void show() {
+        super.show();
+        startGame();
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    private void startGame() {
+        loadLevel();
+    }
+    private void loadLevel() {
+
+    }
+
 }
